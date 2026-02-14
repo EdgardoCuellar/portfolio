dc7e17f190e4259ea76aeb24a91d13c7aa74f1cf
EdgardoCuellar <edgardo-cuellar@hotmail.com>
2026-02-14 10:55:27 +0100

    Updates

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 text-center text-sm text-text/60">
-      © {new Date().getFullYear()} Edgardo Cuellar Sanchez — Contact me for the code :)
-    </footer>
-  );
-}
