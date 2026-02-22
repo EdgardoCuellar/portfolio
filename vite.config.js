29bd4a554721ca979b46f138d68a92f68dd7434f
EdgardoCuellar <edgardo-cuellar@hotmail.com>
2026-02-22 17:24:54 +0100

    Fix crypto.getRandomValues issue with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
+import NodeModulesPolyfillPlugin from 'vite-plugin-node-polyfills';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    NodeModulesPolyfillPlugin()
   ],
-  base: '/',
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      // Ensure these modules are treated as external
+      external: ['crypto'],
+    },
+  },
+});
